c61002c1cf9469ea6af90cd0bdd4301f6bf7b657
Luca Borzacchiello <lucaborza@gmail.com>
2023-02-13 09:58:24 +0100

    fixed mutiny import

--- decept.py
+++ decept.py
@@ -75,11 +75,11 @@ from os import mkdir,getcwd,remove
 
 try:
     sys.path.append(abspath(join(__file__,"../../mutiny_fuzzing_framework"))) 
-    import backend.fuzzer_data as mutiny
+    import backend.fuzzerdata as mutiny
 except ImportError:
     try:
         sys.path.append(abspath(join(__file__, "../../mutiny-fuzzer"))) 
-        import backend.fuzzer_data as mutiny
+        import backend.fuzzerdata as mutiny
     except ImportError:
         pass
 
